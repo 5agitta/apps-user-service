cb835f2e5f32ed38b725f7dfda995b80b8f2a84f
Abir-Hossain <abir.hossain@brainstation-23.com>
2023-11-04 05:03:40 +0600

    Adds convertToDob tests

--- src/test/java/com/sagitta/userservice/UserServiceTest.java
+++ src/test/java/com/sagitta/userservice/UserServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.sagitta.userservice.user.UserService;
 import com.sagitta.userservice.user.UserServiceImpl;
+import com.sagitta.userservice.user.domain.constants.CityCategory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -37,6 +38,44 @@ public class UserServiceTest {
 
         // Verify that the actual age matches the expected age
         assertEquals(expectedAge, age);
+    }
+
+    @Test
+    public void testConvertDOB() throws ParseException {
+        // Define a sample date of birth string
+        String dobString = "15/01/1990";
+
+        // Call the convertDOB method
+        Date dob = userService.convertDOB(dobString);
+
+        // Define the expected Date object based on the sample string
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date expectedDob = dateFormat.parse(dobString);
+
+        // Verify that the actual Date matches the expected Date
+        assertEquals(expectedDob, dob);
+    }
+
+    @Test
+    public void testConvertDOBWithInvalidFormat() {
+        // Define an invalid date of birth string
+        String invalidDobString = "01-15-1990";
+
+        // Call the convertDOB method with an invalid format
+        Date dob = userService.convertDOB(invalidDobString);
+
+        // Verify that the result is null for an invalid format
+        assertEquals(null, dob);
+    }
+
+    @Test
+    public void testGetUserCityCategory() {
+        // Test with valid city category names
+        assertEquals(CityCategory.DHAKA_OR_CHITTAGONG, userService.getUserCityCategory(CityCategory.DHAKA_OR_CHITTAGONG.getName()));
+        assertEquals(CityCategory.OTHER_CITY, userService.getUserCityCategory(CityCategory.OTHER_CITY.getName()));
+
+        // Test with an invalid city category name
+        assertEquals(CityCategory.NON_CITY, userService.getUserCityCategory("InvalidCity"));
     }
 }
 
